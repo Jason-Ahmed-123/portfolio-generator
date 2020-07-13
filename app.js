685194404be6987812d071bff64c349bb5b54332
Jason Ahmed <jason_ahmed@yahoo.com>
2020-07-13 14:57:28 -0700

    dynamically generate webpage via js and node

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
-
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-
+  
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
